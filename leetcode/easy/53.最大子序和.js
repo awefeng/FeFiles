1143fa1313b5989bea111eeb52ee6f89acc2045f
awefeng <awefeng@163.com>
2020-12-31 15:03:26 +0800

    feat: leetcode题解

--- leetcode/easy/53.最大子序和.js
+++ leetcode/easy/53.最大子序和.js
@@ -10,7 +10,17 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-
+    if(nums.length === 0) return 0
+    let pre =0 , sum=nums[0]
+    nums.forEach(item => {
+        if(pre > 0){
+            pre = pre+item 
+        }else{
+            pre = item
+        }
+        sum = Math.max(pre, sum)
+    })
+    return sum
 };
 // @lc code=end
 
